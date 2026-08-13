477c9b5e20999cdb4cd52b0b08ad80febcd054fd
liu-wps <liuyun9786@163.com>
2026-08-13 18:47:06 +0800

    Delete DPWR-DETR-main directory

--- DPWR-DETR-main/DPWR-DETR/dpwr_detr/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Partial reference implementation of the DPWR-DETR core modules."""
-
-from .dird import DIRDDecoderLayer
-from .luph_dgwae import DGWAE, LUPH
-
-__all__ = ("LUPH", "DGWAE", "DIRDDecoderLayer")
